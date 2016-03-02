642c5962e68c8b66f29fcacff2b0390172259aa3
hairuantianxin <734448003@qq.com>
2016-03-02 11:56:29 +0800

    demo2

---
+++ Mvc5demo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc5demo2.MvcApplication" Language="C#" %>
---
+++ Mvc5demo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5demo2.Startup))]
+namespace Mvc5demo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
